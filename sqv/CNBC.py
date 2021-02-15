3fea458549045f8a84f5aacfc71aacd7671c9cbe
Jacob Lee <JLpython@outlook.com>
2021-02-14 20:33:41 -0800

    Create sqv.CNBC module

--- sqv/CNBC.py
+++ sqv/CNBC.py
@@ -1,5 +1,5 @@
 #! python3
-# sqv.py
+# sqv.CNBC
 
 """
 
@@ -7,10 +7,26 @@
 
 import logging
 
+import openpyxl
+
 logging.basicConfig(
     level=logging.INFO,
     format=" %(asctime)s - %(levelname)s - %(message)s"
 )
 
 
+class Parser:
+    """ Parse financial sheet webpages for financial sheet data
+"""
+    def __init__(self, *, report):
+        report = report.lower()
+        if report not in ["annual", "quarter"]:
+            raise self.UnknownReportTypeError(report)
+        self.report = report
 
+    class UnknownReportTypeError(Exception):
+
+        def __init__(self, report):
+            self.report = report
+            self.message = f"No financial sheet '{self.report}'"
+            super().__init__(self.message)
