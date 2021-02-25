77a7b89c306b10ae780c14df1fb0e3772d3c8824
Jacob Lee <JLpython@outlook.com>
2021-02-25 13:29:57 -0800

    Create files for module tests

---
+++ functional_tests.py
@@ -0,0 +1,8 @@
+#! python3
+# functional_tests.py
+
+import unittest
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/YahooFinance.py
@@ -0,0 +1,8 @@
+#! python3
+# tests/YahooFinance.py
+
+import unittest
+
+
+if __name__ == '__main__':
+    unittest.main()
